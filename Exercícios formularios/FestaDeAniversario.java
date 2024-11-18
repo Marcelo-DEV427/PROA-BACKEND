9116e01c4a6f11c04bfb76939bdf7b7a564c0615
Marcelo Alves <marceloalvescorp@gmail.com>
2024-11-17 23:47:00 -0300

    Update FestaDeAniversario.java
    Código estava sem package

--- Exercícios formularios/FestaDeAniversario.java
+++ Exercícios formularios/FestaDeAniversario.java
@@ -1,8 +1,8 @@
 package comandoscondicionais;
 
-// Classe abstrata que define a estrutura de uma Festa de Aniversário
+
 public abstract class FestaDeAniversario {
-    // Atributos
+
     private String tema;
     private int numeroConvidados;
     private String data;
@@ -50,7 +50,7 @@ public abstract class FestaDeAniversario {
         this.orcamento = orcamento;
     }
 
-    // Método abstrato (poderia ser utilizado para adicionar mais funcionalidades)
+
     public abstract void detalhesFesta();
 }
 class FestaDeAniversarioConcreta extends FestaDeAniversario {
@@ -66,28 +66,28 @@ class FestaDeAniversarioConcreta extends FestaDeAniversario {
         System.out.println("Orçamento: R$" + getOrcamento());
     }
 }
-// Classe principal com o método main
+
 class FestaDeAniversarioMain {
 
     public static void main(String[] args) {
-        // Criando uma instância concreta de FestaDeAniversario
+    
         FestaDeAniversarioConcreta festa = new FestaDeAniversarioConcreta();
 
-        // Setando os valores através dos setters
+        
         festa.setTema("Festa Infantil");
         festa.setNumeroConvidados(50);
         festa.setData("15/12/2024");
         festa.setLocal("Salão de Festas Feliz");
         festa.setOrcamento(5000.00);
 
-        // Exibindo os valores através dos getters
+        
         System.out.println("Tema: " + festa.getTema());
         System.out.println("Número de Convidados: " + festa.getNumeroConvidados());
         System.out.println("Data: " + festa.getData());
         System.out.println("Local: " + festa.getLocal());
         System.out.println("Orçamento: R$" + festa.getOrcamento());
 
-        // Chama o método para imprimir os detalhes da festa
+    
         festa.detalhesFesta();
     }
 }
